379c511686af18ba62977e439a0fe38e825a140c
Faigy Mandelbaum <fmandelbaum@integralytic.com>
2023-01-08 12:00:56 +0200

    push

--- OOP/WashingMachine.py
+++ OOP/WashingMachine.py
@@ -1,10 +1,15 @@
+
 class WashingMachine:
+
+    prerequisite = "water_pipe"
+
     def __init__(self, temp, spin_speed, cycle_type, door_is_locked, detergent):
         self.temp = temp
         self.speed = spin_speed
         self.cycle = cycle_type
         self.door_locked = door_is_locked
         self.detergent = detergent
+
     def soak(self):
         if self.door_locked == True:
             print ("I'm soaking")    
@@ -19,13 +24,20 @@ class WashingMachine:
     def agitate(self):
         print ("Scrubbing with " + self.detergent)
 
-    def rinse():
+    def rinse(self):
         print("Rinsing")
 
     def wash_laundry(self):
         self.show_settings()
         self.soak()
         self.agitate()
-        self.rinse()                
+        self.rinse() 
+
+my_washing_machine = WashingMachine(30, 600, "cottons", True, "ariel")
+
+my_broken_washing_machune = WashingMachine(60, 0, "wool", False, "Persil Gel")
+
+my_washing_machine.wash_laundry()
+
 
         
